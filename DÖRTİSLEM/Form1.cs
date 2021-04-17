603e91412821936285c5463d915c8b7d47ea0aed
arman <arman_a2002@hotmail.com>
2021-04-18 00:40:12 +0300

    Komit1

--- DÖRTİSLEM/Form1.cs
+++ DÖRTİSLEM/Form1.cs
@@ -16,5 +16,18 @@ namespace DÖRTİSLEM
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int x = Convert.ToInt32(textBox1.Text.ToString());
+            int y = Convert.ToInt32(textBox2.Text.ToString());
+            int toplam = x + y;
+            MessageBox.Show("" + toplam);
+        }
     }
 }
